292fc797c3e1bd471d8673bfb19b58f6a697a8b0
muntamehtaz <munta.mehtaz@concordusa.com>
2023-09-22 13:32:09 -0500

    adding Mongo CLI based logic to ProductRepository class

--- src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -20,6 +20,7 @@ namespace Catalog.API.Repositories
                 .Find(p => true)
                 .ToListAsync();
         }
+
         public async Task<Product> GetProduct(string id)
         {
             return await _context
@@ -27,9 +28,10 @@ namespace Catalog.API.Repositories
                 .Find(p => p.Id == id)
                 .FirstOrDefaultAsync();
         }
+
         public async Task<IEnumerable<Product>> GetProductByName(string name)
         {
-            FilterDefinition<Product> filter = Builders<Product>.Filter.ElemMatch(p => p.Name, name);
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Name, name);
 
             return await _context
                 .Products
@@ -37,25 +39,39 @@ namespace Catalog.API.Repositories
                 .ToListAsync();
         }
 
-        public Task CreateProduct(Product product)
+        public async Task<IEnumerable<Product>> GetProductByCategory(string category)
         {
-            throw new NotImplementedException();
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Category, category);
+
+            return await _context
+                .Products
+                .Find(filter)
+                .ToListAsync();
         }
 
-        public Task<bool> DeleteProduct(string id)
+        public async Task CreateProduct(Product product)
         {
-            throw new NotImplementedException();
+            await _context.Products.InsertOneAsync(product);
         }
 
-        public Task<IEnumerable<Product>> GetProductByCategory(string category)
+        public async Task<bool> UpdateProduct(Product product)
         {
-            throw new NotImplementedException();
+            var updateResult = await _context
+                .Products
+                .ReplaceOneAsync(filter: g => g.Id == product.Id, replacement: product);
+
+            return updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
         }
 
-
-        public Task<bool> UpdateProduct(Product product)
+        public async Task<bool> DeleteProduct(string id)
         {
-            throw new NotImplementedException();
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Id, id);
+
+            DeleteResult deleteResult = await _context
+                .Products
+                .DeleteOneAsync(filter);
+
+            return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
         }
     }
 }
