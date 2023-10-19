d482c91850b37ace35a2c8511365aa9ea56f46d9
muntamehtaz <munta.mehtaz@concordusa.com>
2023-10-19 15:14:57 -0500

    add folder structure to Ordering.Application project

---
+++ src/Services/Ordering/Ordering.Application/Mapper/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() {
+            //CreateMap<Order, OrdersVm>().ReverseMap();
+            //CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            //CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+        }
+    }
+}
